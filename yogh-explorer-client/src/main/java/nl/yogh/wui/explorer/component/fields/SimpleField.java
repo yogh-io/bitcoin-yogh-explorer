858494bce9ffc7ecd0b87fe0af4c9fe0002ebeaa
yogh-io <info@yogh.nl>
2020-12-28 10:57:25 +0100

    More field formatting

--- yogh-explorer-client/src/main/java/nl/yogh/wui/explorer/component/fields/SimpleField.java
+++ yogh-explorer-client/src/main/java/nl/yogh/wui/explorer/component/fields/SimpleField.java
@@ -2,10 +2,16 @@ package nl.yogh.wui.explorer.component.fields;
 
 import com.axellience.vuegwt.core.annotations.component.Component;
 import com.axellience.vuegwt.core.annotations.component.Prop;
+import com.axellience.vuegwt.core.annotations.component.PropDefault;
 import com.axellience.vuegwt.core.client.component.IsVueComponent;
 
 @Component
 public class SimpleField implements IsVueComponent {
   @Prop String label;
   @Prop String value;
+  
+  @PropDefault("value")
+  String valueDefault() {
+    return "";
+  }
 }
--- yogh-explorer-client/src/main/java/nl/yogh/wui/explorer/ui/transaction/TransactionView.java
+++ yogh-explorer-client/src/main/java/nl/yogh/wui/explorer/ui/transaction/TransactionView.java
@@ -9,6 +9,9 @@ import com.axellience.vuegwt.core.annotations.component.Prop;
 import com.axellience.vuegwt.core.client.component.IsVueComponent;
 import com.google.web.bindery.event.shared.EventBus;
 
+import jsinterop.annotations.JsMethod;
+
+import nl.yogh.wui.explorer.component.fields.SimpleField;
 import nl.yogh.wui.explorer.component.hex.HexViewer;
 import nl.yogh.wui.explorer.component.hex.interpreters.TransactionInterpreter;
 import nl.yogh.wui.explorer.component.links.AddressLink;
@@ -18,12 +21,14 @@ import nl.yogh.wui.explorer.context.BlockContext;
 import nl.yogh.wui.explorer.context.TransactionContext;
 import nl.yogh.wui.explorer.service.ElectrServiceAsync;
 import nl.yogh.wui.explorer.service.domain.TransactionInformation;
+import nl.yogh.wui.util.EllipsisUtil;
 
 @Component(components = {
     BlockLink.class,
     TransactionLink.class,
     AddressLink.class,
-    HexViewer.class
+    HexViewer.class,
+    SimpleField.class
 })
 public class TransactionView implements IsVueComponent {
   @Prop EventBus eventBus;
@@ -32,16 +37,21 @@ public class TransactionView implements IsVueComponent {
 
   @Data @Inject TransactionContext context;
   @Data @Inject BlockContext blockContext;
-  
+
   @Data @Inject TransactionInterpreter txInterpreter;
 
   @Computed
   public TransactionInformation getTransaction() {
     return context.transactionInformation;
   }
-  
+
   @Computed
   public String getRaw() {
     return context.raw;
+  }
+
+  @JsMethod
+  public String formatPayload(final String payload) {
+    return EllipsisUtil.applyInnerPayload(payload);
   }
 }
